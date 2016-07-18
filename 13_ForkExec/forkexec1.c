751a0136016ad809553fc9c04cde74374bc29622
tctsigemura <sigemura@tokuyama.ac.jp>
2016-07-18 22:59:56 +0900

    第１３回完成

---
+++ 13_ForkExec/forkexec1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>                   // exit のために必要
+#include <unistd.h>                   // fork, execve のために必要
+#include <sys/wait.h>                 // wait のために必要
+
+extern char **environ;
+char *args[] = {"date", NULL};
+char *execpath="/bin/date";
+
+int main(int argc, char *argv[], char *envp[]) {
+  int pid;
+  if ((pid=fork())<0) {               // 分身を作る
+    perror(argv[0]);                  // fork がエラーなら
+    exit(1);                          // 親プロセスをエラー終了
+  }
+  if (pid!=0) {                       // pid が 0 以外なら自分は親プロセス
+    int status;
+    wait(&status);                    //   子プロセスが終了するのを待つ
+  } else {                            // pid が 0 なら自分は子プロセス
+    execve(execpath, args, environ);  //   date プログラムを実行
+    perror(execpath);                 //   exec が戻ってくるならエラー
+    exit(1);                          //   エラー時はここで子プロセスを終了
+  }
+  exit(0);                            // 親プロセスを正常終了
+}
